52948813cfbe3963fb770b313bedc1d7079f2632
Takeshi MATSUYA <tmatsuya@matcchan-no-MacBook-Air.local>
2014-05-29 01:53:13 +0900

    frame len is 68

--- boards/kc705/test/tb_system.v
+++ boards/kc705/test/tb_system.v
@@ -56,7 +56,7 @@ measure measure_inst (
 	.tx0_ipv6(1'b0),
 	.tx0_fullroute(1'b0),
 	.tx0_req_arp(1'b0),
-	.tx0_frame_len(16'd64),
+	.tx0_frame_len(16'd68),
 	.tx0_inter_frame_gap(32'd12),
 	.tx0_ipv4_srcip({8'd192, 8'd168, 8'd1, 8'd101}),
 	.tx0_src_mac(48'h554433_221100),
